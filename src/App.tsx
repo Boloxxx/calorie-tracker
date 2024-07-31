f66e85619080b5cd777b5138ebfea534b657042b
Boloxxx <bryanrodriguezalmonte@gmail.com>
2024-07-31 16:29:00 -0400

    v.02

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
+import { useReducer } from "react"
+import Form from "./components/Form"
+import { activityReducer, initialState } from "./reducers/activity-reducer"
 
-import './App.css'
 
 function App() {
 
+  const [state, dispatch] = useReducer(activityReducer, initialState)
+
   return (
     <>
-      <div>
-        <h1>
-          Rodriguez Muebles
-        </h1>
+      <header className="bg-lime-600 py-3">
+        <div className="max-w-4xl mx-auto flex justify-between">
+          <h1 className="text-center text-lg font-bold text-white uppercase">
+            Contador de Calorias
+          </h1>
 
-        <h3>En construccion....</h3>
-      </div>
+
+        </div>
+      </header>
+
+      <section className="bg-lime-500 py-20 px-5">
+        <div className="max-w-4xl mx-auto">
+          <Form
+            dispatch={dispatch}
+          />
+        </div>
+      </section>
     </>
   )
 }
