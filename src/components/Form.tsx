56f71b42ffc8fe068bb6934de1270f311538ea20
Boloxxx <bryanrodriguezalmonte@gmail.com>
2024-08-01 16:48:31 -0400

    v.0102

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,43 +1,53 @@
 import { useState, ChangeEvent, FormEvent, Dispatch } from "react";
+import { v4 as uuidv4 } from 'uuid'
 import { Activity } from "../types";
 import { categories } from "../data/categories";
 import { ActivityActions } from "../reducers/activity-reducer";
 
 type FormProps = {
-    dispatch: Dispatch<ActivityActions>
-}
+  dispatch: Dispatch<ActivityActions>;
+};
 
-const Form = ({dispatch}) => {
-  const [activity, setActivity] = useState<Activity>({
-    category: 1,
-    name: "",
-    calories: 0,
-  });
+const initialState: Activity = {
+  id: uuidv4(),
+  category: 1,
+  name: "",
+  calories: 0,
+};
 
-  const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement> ) => {
-    const isNumberField = ['category', 'calories'].includes(e.target.id)
-    
+function Form({ dispatch }): FormProps {
+  const [activity, setActivity] = useState<Activity>(initialState);
+
+  const handleChange = (
+    e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>
+  ) => {
+    const isNumberField = ["category", "calories"].includes(e.target.id);
+
     setActivity({
       ...activity,
-      [e.target.id]: isNumberField ? +e.target.value : e.target.value
+      [e.target.id]: isNumberField ? +e.target.value : e.target.value,
     });
-
   };
 
   const isValidActivity = () => {
-    const { name, calories } = activity
-    return name.trim() !== '' && calories > 0
-  }
+    const { name, calories } = activity;
+    return name.trim() !== "" && calories > 0;
+  };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-    dispatch({type: "save-activity", payload: {newActivity: activity}})
-  }
+    dispatch({ type: "save-activity", payload: { newActivity: activity } });
+    setActivity({
+      ...initialState,
+      id: uuidv4()
+    });
+  };
 
   return (
-    <form className="space-y-5 bg-white shadow p-10 rounded-lg"
-    onSubmit={handleSubmit}
+    <form
+      className="space-y-5 bg-white shadow p-10 rounded-lg"
+      onSubmit={handleSubmit}
     >
       <div className="grid grid-cols-1 gap-3">
         <label htmlFor="category" className="font-bold">
@@ -88,11 +98,11 @@ const Form = ({dispatch}) => {
       <input
         type="submit"
         className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer disabled:opacity-10"
-        value={activity.category === 1 ? 'Guardar Comida' : 'Guardar Ejercicio'}
+        value={activity.category === 1 ? "Guardar Comida" : "Guardar Ejercicio"}
         disabled={!isValidActivity()}
       />
     </form>
   );
-};
+}
 
 export default Form;
