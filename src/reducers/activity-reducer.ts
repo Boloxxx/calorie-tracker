688247ffe4588dc921092dd63c3e22838894ce96
Boloxxx <bryanrodriguezalmonte@gmail.com>
2024-08-06 16:25:51 -0400

    v.01

--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -1,38 +1,70 @@
-import { Activity } from "../types"
+import { Activity } from "../types";
 
-export type ActivityActions = 
-{ type: 'save-activity', payload: { newActivity : Activity } } |
-{ type: 'set-activeId', payload: { id : Activity['id'] } } 
+export type ActivityActions =
+  | { type: "save-activity"; payload: { newActivity: Activity } }
+  | { type: "set-activeId"; payload: { id: Activity["id"] } }
+  | { type: "delete-activity"; payload: { id: Activity["id"] } };
+    { type: "restart-app"} 
 
-type ActivityState = {
-    activities: Activity[],
-    activeId: Activity['id']
+export type ActivityState = {
+  activities: Activity[];
+  activeId: Activity["id"];
+};
+
+const localStorageActivities = () : Activity[] => {
+    const activities = localStorage.getItem('activities')
+    return activities ? JSON.parse(activities) : []
 }
 
-export const initialState : ActivityState = {
-    activities: [],
-    activeId: ''
-}
+export const initialState: ActivityState = {
+  activities: localStorageActivities(),
+  activeId: "",
+};
 
 export const activityReducer = (
-    state : ActivityState = initialState,
-    action : ActivityActions
+  state: ActivityState = initialState,
+  action: ActivityActions
 ) => {
-    if(action.type === 'save-activity') {
-        // Este codigo maneja la logica para actualizar el state
-        
-        return {
-            ...state,
-            activities: [...state.activities, action.payload.newActivity]
-        }
-    }
+  if (action.type === "save-activity") {
+    // Este codigo maneja la logica para actualizar el state
 
-    if(action.type === 'set-activeId') {
-        return {
-            ...state,
-            activeId: action.payload.id
-        }
+    let updatedActivities: Activity[] = [];
+    if (state.activeId) {
+      updatedActivities = state.activities.map((activity) =>
+        activity.id === state.activeId ? action.payload.newActivity : activity
+      );
+    } else {
+      updatedActivities = [...state.activities, action.payload.newActivity];
     }
+    return {
+      ...state,
+      activities: updatedActivities,
+      activeId: "",
+    };
+  }
 
-    return state
-}
+  if (action.type === "set-activeId") {
+    return {
+      ...state,
+      activeId: action.payload.id,
+    };
+  }
+
+  if (action.type === "delete-activity") {
+    return {
+      ...state,
+      activities: state.activities.filter(
+        (activity) => activity.id !== action.payload.id
+      ),
+    };
+  }
+
+  if(action.type === 'restart-app') {
+    return {
+        activities: [],
+        activeId: ''
+    }
+  }
+
+  return state;
+};
